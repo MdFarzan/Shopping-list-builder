1aedcd4a3d297cc142df5c07ceba776029ce4bb3
MdFarzan <mdfarzan776@gmail.com>
2020-06-05 07:05:40 +0530

    input list name functionality finished.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("js is perfectly referenced.");
+document.body.onload = function(){
+    document.querySelector("#name-of-list").style.display = "block";
+    document.querySelector("main").style.opacity = "0.3";
+}
+
+document.getElementById("create-list").addEventListener("click",()=>{
+    let listName = document.querySelector("input[name=list-name]").value;
+    document.querySelector("#list-name").innerText = listName;
+    document.querySelector("#name-of-list").style.display = "none";
+    displayBody();
+});
+
+//body displayer
+function displayBody(){
+    document.querySelector("main").style.opacity = "1";
+}
